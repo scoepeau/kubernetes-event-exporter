569248a30f996f15a02d8749476b0da9b7727478
Stella Coëpeau <31846959+scoepeau@users.noreply.github.com>
2021-11-19 14:24:05 +0100

    fix: remove cache

--- pkg/kube/annotations.go
+++ pkg/kube/annotations.go
@@ -15,28 +15,18 @@ type AnnotationCache struct {
 	dynClient dynamic.Interface
 	clientset *kubernetes.Clientset
 
-	cache *lru.ARCCache
 	sync.RWMutex
 }
 
 func NewAnnotationCache(kubeconfig *rest.Config) *AnnotationCache {
-	cache, err := lru.NewARC(1024)
-	if err != nil {
-		panic("cannot init cache: " + err.Error())
-	}
 	return &AnnotationCache{
 		dynClient: dynamic.NewForConfigOrDie(kubeconfig),
 		clientset: kubernetes.NewForConfigOrDie(kubeconfig),
-		cache:     cache,
 	}
 }
 
 func (a *AnnotationCache) GetAnnotationsWithCache(reference *v1.ObjectReference) (map[string]string, error) {
 	uid := reference.UID
-
-	if val, ok := a.cache.Get(uid); ok {
-		return val.(map[string]string), nil
-	}
 
 	obj, err := GetObject(reference, a.clientset, a.dynClient)
 	if err == nil {
@@ -46,13 +36,10 @@ func (a *AnnotationCache) GetAnnotationsWithCache(reference *v1.ObjectReference)
 				delete(annotations, key)
 			}
 		}
-		a.cache.Add(uid, annotations)
 		return annotations, nil
 	}
 
 	if errors.IsNotFound(err) {
-		var empty map[string]string
-		a.cache.Add(uid, empty)
 		return nil, nil
 	}
 
